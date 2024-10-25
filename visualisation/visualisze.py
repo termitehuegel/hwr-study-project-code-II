0b7ec9e58e62b477c37e21f20a89cc43b06c6ba6
Philip Berner <philipberner.berlin@gmail.com>
2024-10-25 11:35:50 +0200

    visualisation script

---
+++ visualisation/visualisze.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+df['start_time'] = pd.to_datetime(df['start_time'])
+df['end_time'] = pd.to_datetime(df['end_time'])
+
+
+df['micro_to_start'] = (df['start_time'] - df['start_time'].min()).dt.microseconds
+df['micro_to_end'] = (df['end_time'] - df['start_time'].min()).dt.microseconds
+df['task_duration'] = df['micro_to_end'] - df['micro_to_start'] + 1
+
+plt.barh(y=df['table_char'], width=df['task_duration'], left=df['micro_to_start'])
+plt.show()
